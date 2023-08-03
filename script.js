4ad39efdca9545e02331f18aee927a065402f391
AndSim <siminiucandrew@gmail.com>
2023-08-03 17:18:46 +0300

    feat: implement a tabbed component in operations section. when click on one of 3 tabs then the content of this area below will change.

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function (e) {
     e.preventDefault();
@@ -27,8 +32,7 @@ document.addEventListener("keydown", function (e) {
     }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+
 btnScrollTo.addEventListener('click', function (e) {
     section1.scrollIntoView({behavior: 'smooth'});
 });
@@ -40,4 +44,16 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
         const id = e.target.getAttribute('href');
         document.querySelector(id).scrollIntoView({behavior: 'smooth'});
     }
+});
+
+tabsContainer.addEventListener('click', function (e) {
+    const clicked = e.target.closest('.operations__tab');
+
+    if (!clicked) return;
+
+    tabs.forEach(t => t.classList.remove('operations__tab-active'));
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+    clicked.classList.add('operations__tab-active');
+
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
